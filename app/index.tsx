d8442e073dcd028b349cbaadfe720a0735d5caca
kuman514 <hadjadj0@gmail.com>
2025-01-09 08:38:37 +0900

    chore: remove unnecessary modules and files

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Pressable, Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Pressable>
+        <Text>Apple Signin</Text>
+      </Pressable>
+    </View>
+  );
+}
